1c94222daf5f72f0e3c9a30d27dc2ee9ce9d0785
tdvx <59688080+tordevries@users.noreply.github.com>
2025-02-12 16:34:58 -0500

    added js for timeout

--- scripts.js
+++ scripts.js
@@ -1,5 +1,7 @@
 document.addEventListener("DOMContentLoaded", function(event){
-
     document.getElementById("helloTest").innerHTML = "testing: 1, 2, 3... ";
-
+    setTimeout(function() {
+        var userResponse = prompt("Welcome to the page! Please enter your response:");
+        console.log("User response:", userResponse);
+    }, 1000);
 });
