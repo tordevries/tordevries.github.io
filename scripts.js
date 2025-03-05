25ebce32f0abcb8fd850929fd0ac1bd54c5cacdd
tdvx <59688080+tordevries@users.noreply.github.com>
2025-03-05 15:57:32 -0500

    removed test function

--- scripts.js
+++ scripts.js
@@ -1,7 +1,1 @@
-document.addEventListener("DOMContentLoaded", function(event){
-    document.getElementById("helloTest").innerHTML = "testing: 1, 2, 3... ";
-    setTimeout(function() {
-        var userResponse = prompt("Welcome to the page! Please enter your response:");
-        console.log("User response:", userResponse);
-    }, 1000);
-});
+/* nothing here right now */
