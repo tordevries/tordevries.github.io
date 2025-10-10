d59fb6c6fe9da9e83985eab53104480517e6def5
tdvx <59688080+tordevries@users.noreply.github.com>
2025-10-10 19:50:44 -0400

    added clicked css and js

--- responsive-menu-demo/script.js
+++ responsive-menu-demo/script.js
@@ -1,1 +1,13 @@
 // JS for responsive menu demo
+
+// wait until the DOM is loaded before we can start adding event listeners
+document.addEventListener("DOMContentLoaded", function(){
+
+    // respond to clicks on the burger icon
+    document.querySelector("#navBurger").addEventListener("clicked", function(e){
+        document.querySelector("nav").classList.toggle("clicked");
+    });
+
+
+
+});
