eae7b1e589730dfbceda1f409847eb94b167a881
tdvx <59688080+tordevries@users.noreply.github.com>
2025-10-10 19:59:16 -0400

    fixed JS typo

--- responsive-menu-demo/script.js
+++ responsive-menu-demo/script.js
@@ -4,7 +4,7 @@
 document.addEventListener("DOMContentLoaded", function(){
 
     // respond to clicks on the burger icon
-    document.querySelector("#navBurger").addEventListener("clicked", function(e){
+    document.querySelector("#navBurger").addEventListener("click", function(e){
         document.querySelector("nav").classList.toggle("clicked");
     });
 
