ec008278d2b482e4f29ca21432b8197813f1dadd
tdvx <59688080+tordevries@users.noreply.github.com>
2025-10-10 20:32:36 -0400

    tweaked JS

--- responsive-menu-demo/script.js
+++ responsive-menu-demo/script.js
@@ -40,7 +40,9 @@ document.addEventListener("DOMContentLoaded", function(){
   
   // close nav if someone clicks outside nav
   document.querySelector("html").addEventListener("click", function(e) {
-    if ( !e.target.closest("nav ul") && document.querySelector("nav").classList.contains("clicked") ) {
+    console.log("1");
+    if ( !e.target.closest("nav") && document.querySelector("nav").classList.contains("clicked") ) {
+        console.log("2");
       clickNav(true);
     }
   });
