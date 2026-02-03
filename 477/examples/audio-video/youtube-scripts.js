4a9caedcf0e580805442ff05ee8120dfb81dea00
Tor de Vries <tor.devries@wsu.edu>
2026-02-03 17:06:44 +0000

    reorganized HTML in the YouTube example

---
+++ 477/examples/audio-video/youtube-scripts.js
@@ -0,0 +1,74 @@
+// This code loads the IFrame Player API code asynchronously.
+let tag = document.createElement('script');
+
+tag.src = "https://www.youtube.com/iframe_api";
+let firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+// This function creates an <iframe> (and YouTube player)
+//    after the API code downloads.
+let player;
+function onYouTubeIframeAPIReady() {
+    player = new YT.Player('player', {
+        height: '390',
+        width: '640',
+        videoId: '-2MUYzkisCM',
+        events: {
+            'onReady': onPlayerReady,
+            'onStateChange': onPlayerStateChange
+        }
+    });
+}
+
+// The API will call this function when the video player is ready.
+function onPlayerReady(event) {
+    // event.target.playVideo();  // commented out so it doesn't autoplay
+    document.getElementById("vidStatus").innerHTML = "Ready";
+}
+
+// The API calls this function when the player's state changes.
+// The function indicates that when playing a video (state=1),
+// the player should play for six seconds and then stop.
+var done = false;
+function onPlayerStateChange(event) {
+    if (event.data == YT.PlayerState.PLAYING && !done) {
+        setTimeout(stopVideo, 6000);
+        done = true;
+    }
+
+    if (event.data == 2) {
+        document.getElementById("vidStatus").innerHTML = "Paused";
+    }
+
+    if (event.data == 1) {
+        document.getElementById("vidStatus").innerHTML = "Playing";
+    }
+
+}
+function stopVideo() {
+    player.stopVideo();
+}
+
+
+// function to play the video using the object from window.onload
+function playVid() {
+    player.playVideo();
+}
+
+// function to pause the video using the object from window.onload
+function pauseVid() {
+    player.pauseVideo()
+}
+
+
+// wait for window to load, then get info and attach click events
+window.addEventListener("load", function() {
+    
+    // get video URL
+    document.querySelector("#vidURL").innerHTML = player.getVideoUrl();
+
+    // add event listeners to call functions that start and stop audio
+    document.querySelector("#youPlay").addEventListener("click", playVid);	
+    document.querySelector("#youPause").addEventListener("click", pauseVid);	
+
+});
