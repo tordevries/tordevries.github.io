604032dd66f15c9661af7dcc419cc1d9e1af2d27
Tor de Vries <tor.devries@wsu.edu>
2026-03-18 18:51:50 +0000

    updated madlibs hack

--- 477/examples/modal-madlibs/scripts.js
+++ 477/examples/modal-madlibs/scripts.js
@@ -1,20 +1,20 @@
 // javascript for modal example
 
-
+// toggle modal display by toggling class on body, which will trigger
+// css styles around body.showModal 
 function toggleModal() {
-	document.querySelector("#modalCover").classList.toggle("hidden");
-	document.querySelector("#modalPopup").classList.toggle("hidden");
+	document.body.classList.toggle("showModal");
 }
 
 
 // wait for DOM to load before adding events
 document.addEventListener("DOMContentLoaded", function() {
 
-	// show modal
+	// handle the modal form's submit event
 	let formToSubmit = document.querySelector("#madlibsForm");
 	formToSubmit.addEventListener("submit", function(e) {
 
-		e.preventDefault(); // don't let form submit
+		e.preventDefault(); // don't let form submit to server
 
 		// examine form code values
 		let adj1 = document.querySelector("#adjective1").value;
@@ -22,13 +22,13 @@ document.addEventListener("DOMContentLoaded", function() {
 		let noun1 = document.querySelector("#noun1").value;
 		let noun2 = document.querySelector("#noun2").value;
 		let year1 = document.querySelector("#year1").value;
-		let extra1 = document.querySelector("#extra1").checked;
+		let extra1 = document.querySelector("#extra1").checked; // boolean value
 
-		// build madlib output string
+		// build madlib output string using template strings
 		let outputText = `A vacation is when you take a trip to some ${adj1}
-						  place with your ${adj2} family. Usually you go to some
-						  place near ${noun1} or up on ${noun2}.
-						  A good vacation is when you spent ${year1} years there.`;
+						  place with your ${adj2} family. Usually you visit
+						  ${noun1} or go look at ${noun2}. A good vacation is 
+						  when you spent ${year1} years there.`;
 
 		if (extra1) {
 			outputText += " Let's go on vacation!";
@@ -40,7 +40,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
 	});
 
-	// hide modal
+	// button to show/hide modal
 	let hideIt = document.querySelector("#setMadlib");
 	hideIt.addEventListener("click", toggleModal);
 
