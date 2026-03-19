a3b03f638d5430fa130bdc2fb507a186f9afca8b
Tor de Vries <tor.devries@wsu.edu>
2026-03-19 17:42:26 +0000

    added sample API output

---
+++ 477/examples/ajax-api-test/current.js
@@ -0,0 +1,47 @@
+{
+  "location": {
+    "name": "Pullman",
+    "region": "Washington",
+    "country": "United States of America",
+    "lat": 46.7314,
+    "lon": -117.1786,
+    "tz_id": "America/Los_Angeles",
+    "localtime_epoch": 1773942138,
+    "localtime": "2026-03-19 10:42"
+  },
+  "current": {
+    "last_updated_epoch": 1773941400,
+    "last_updated": "2026-03-19 10:30",
+    "temp_c": 12.8,
+    "temp_f": 55,
+    "is_day": 1,
+    "condition": {
+      "text": "Overcast",
+      "icon": "//cdn.weatherapi.com/weather/64x64/day/122.png",
+      "code": 1009
+    },
+    "wind_mph": 10.5,
+    "wind_kph": 16.9,
+    "wind_degree": 216,
+    "wind_dir": "SW",
+    "pressure_mb": 1022,
+    "pressure_in": 30.17,
+    "precip_mm": 0,
+    "precip_in": 0,
+    "humidity": 64,
+    "cloud": 100,
+    "feelslike_c": 11.2,
+    "feelslike_f": 52.1,
+    "windchill_c": 12.8,
+    "windchill_f": 55,
+    "heatindex_c": 13.5,
+    "heatindex_f": 56.3,
+    "dewpoint_c": 3.9,
+    "dewpoint_f": 39,
+    "vis_km": 16,
+    "vis_miles": 9,
+    "uv": 0.5,
+    "gust_mph": 15.3,
+    "gust_kph": 24.6
+  }
+}
